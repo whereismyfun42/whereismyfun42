d3cafb41cfac3e05ee4dce4db191514c319f1e43
whereismyfun42 <whereismyfun42@gmail.com>
2024-08-12 16:47:52 +0200

    revert shader

--- main.js
+++ main.js
@@ -20,147 +20,13 @@ camera.position.setX(-3);
 
 renderer.render(scene, camera);
 
-// Pentagram Shader
+// Torus
 
-const pentagramFragmentShader = `
-  const float M_PI = 3.141592;
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const torus = new THREE.Mesh(geometry, material);
 
-  // Signed distance functions
-  float Box(in vec3 p, in vec3 b) {
-      vec3 d = abs(p) - b;
-      return min(max(d.x, max(d.y, d.z)), 0.0) + length(max(d, 0.0));
-  }
-
-  float Cylinder(in vec3 p, in float r, in float h) {
-      return max(length(p.xz) - r, abs(p.y) - h);
-  }
-
-  float Subtract(in float a, in float b) {
-      return max(a, -b);
-  }
-
-  float Union(in float d1, in float d2) {
-      return min(d1, d2);
-  }
-
-  float Intersect(in float d1, in float d2) {
-      return max(d1, d2);
-  }
-
-  // Rotation matrices
-  mat3 rotX(in float a) {
-      return mat3(1.0, 0.0, 0.0,
-                  0.0, cos(a), -sin(a),
-                  0.0, sin(a),  cos(a));
-  }
-
-  mat3 rotY(in float a) {
-      return mat3(cos(a), 0.0, sin(a),
-                     0.0, 1.0, 0.0,
-                 -sin(a), 0.0, cos(a));
-  }
-
-  mat3 rotZ(in float a) {
-      return mat3(cos(a), -sin(a), 0.0,
-                  sin(a),  cos(a), 0.0,
-                     0.0,     0.0, 1.0);      
-  }
-
-  // SDF pentagram composition
-  float Pentagram(in vec3 p) {
-      // pentagram octagonal frame
-      mat3 r1 = rotZ(0.12 * M_PI);
-      mat3 r2 = rotZ(0.38 * M_PI);
-      float a = Box(p * r1, vec3(0.6, 0.6, 0.15));
-      float b = Box(p * r2, vec3(0.6, 0.6, 0.15));
-      float c = Box(p * r1, vec3(0.7, 0.7, 0.05));
-      float d = Box(p * r2, vec3(0.7, 0.7, 0.05));
-
-      float frame = Subtract(Intersect(c, d), Intersect(a, b));
-
-      // inside of the pentagram
-      vec3 offset = vec3(0.22, 0.0, 0.0);
-      float star  = Cylinder(p * rotZ(-0.5 * M_PI) + vec3(0.17, 0.0, 0.0), 0.04, 0.61);
-      star = Union(star, Cylinder(p * rotZ(0.115 * M_PI) - offset, 0.04, 0.61 ));
-      star = Union(star, Cylinder(p * rotZ(-.115 * M_PI) + offset, 0.04, 0.61 ));
-      star = Union(star, Cylinder(p * rotZ(-0.68 * M_PI) - offset, 0.04, 0.61 ));
-      star = Union(star, Cylinder(p * rotZ(-0.32 * M_PI) - offset, 0.04, 0.61 ));
-
-      return Union(frame, star);
-  }
-
-  // Surface normal
-  vec3 SurfaceNormal(in vec3 pos, in mat3 matrix) {
-      vec3 eps = vec3( 0.001, 0.0, 0.0 );
-      return normalize(-vec3(Pentagram((pos + eps.xyy) * matrix) - Pentagram((pos - eps.xyy) * matrix),
-                             Pentagram((pos + eps.yxy) * matrix) - Pentagram((pos - eps.yxy) * matrix),
-                             Pentagram((pos + eps.yyx) * matrix) - Pentagram((pos - eps.yyx) * matrix)));
-  }
-
-  // Raymarching function
-  float Raymarch(in vec3 from, in vec3 to, in mat3 matrix) {
-      const float MIN_DIST = 0.001;
-      const float MAX_DIST = 3.0;
-      const int NUM_STEPS = 50;
-      float dist = MIN_DIST;
-      float depth = 0.0;
-
-      for(int i = 0; i < NUM_STEPS; ++i) {
-          if(dist < MIN_DIST || depth > MAX_DIST) 
-              break;
-
-          dist  = Pentagram((from + to * depth) * matrix);
-          depth += dist;
-
-          if(depth > MAX_DIST)
-              return 0.0;
-      }
-
-      return depth;
-  }
-
-  void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-      float ratio = iResolution.x / iResolution.y;
-      vec2 uv  = fragCoord.xy / iResolution.xy;
-      vec2 pos = 2.0 * uv - 1.0;
-      pos.x   *= ratio;
-
-      vec3 from  = vec3(0.0, 0.0, -1.666);
-      vec3 to    = normalize(vec3(pos.xy, 1.666));
-      vec3 color = vec3(0.0);
-
-      mat3 transform = rotY(iTime);
-
-      float t = Raymarch(from, to, transform);
-
-      if(t > 0.0) {
-          vec3 p = from + t * to;
-          vec3 n = SurfaceNormal(p, transform);
-
-          vec3 tx = texture(iChannel0, p.yz + 0.1666 * iTime).xyz * n.x * 4.666;
-          vec3 ty = texture(iChannel0, p.xz + 0.2666 * iTime).xyz * n.y * 3.666;
-          vec3 tz = texture(iChannel0, p.xy + 0.3666 * iTime).xyz * n.z * 2.666;
-
-          color.r  = clamp((tx + ty + tz) * 0.333, 0.0, 1.0).r;
-          color.gb = vec2(0.0);
-      }
-
-      fragColor = vec4(color, 1.0);
-  }
-`;
-
-const pentagramMaterial = new THREE.ShaderMaterial({
-  fragmentShader: pentagramFragmentShader,
-  uniforms: {
-    iTime: { value: 0 },
-    iResolution: { value: new THREE.Vector3() },
-  },
-});
-
-const pentagramGeometry = new THREE.PlaneGeometry(5, 5);
-
-const pentagramMesh = new THREE.Mesh(pentagramGeometry, pentagramMaterial);
-scene.add(pentagramMesh);
+scene.add(torus);
 
 // Lights
 
@@ -252,9 +118,9 @@ moveCamera();
 function animate() {
   requestAnimationFrame(animate);
 
-  // Update pentagram uniform
-  pentagramMaterial.uniforms.iTime.value += 0.05;
-  pentagramMaterial.uniforms.iResolution.value.set(window.innerWidth, window.innerHeight, 1);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
 
   moon.rotation.x += 0.005;
 
