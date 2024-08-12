0d631fb2689ff6d22cf22f794dd392f181b1b34a
whereismyfun42 <whereismyfun42@gmail.com>
2024-08-12 14:32:10 +0200

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import vue from "@vitejs/plugin-vue"
 
 export default defineConfig({
     base: '/whereismyfun42/',
-    plugins: [vue()]
+    plugins: [vue()],
+    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif'],  // Ensure these assets are included in the build
+
 })
